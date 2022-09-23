0e37c43b84f0db9a3a208b0c352bcac07845bd0f
VASSUNARESH <nareshkumar050396@gmail.com>
2022-09-23 16:59:10 +0530

    mathematical_operator

---
+++ mathematical-operator-iw (1)/problem2.js
@@ -0,0 +1,39 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five= 5;
+console.log(1 *2+ 2 *2+ 3 *2+ 4 *2+ 5*2 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mathematical-operator-iw (1)/problem3.js
@@ -0,0 +1,8 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five= 5;
+console.log(1 + 2 *2+ 3 *3+ 4 *3+ 5*2+6*2)
+
+
